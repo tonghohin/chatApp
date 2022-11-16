4fb165b04b2ff0ca50d488314c4b636da0693f80
Hin <tonghohin@hotmail.com>
2022-11-16 09:42:11 -0400

    cors

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ server.listen(process.env.PORT || 3000, () => {
 app.set("view engine", "ejs");
 app.use(express.static(__dirname));
 app.use(sessionMiddleware);
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 
